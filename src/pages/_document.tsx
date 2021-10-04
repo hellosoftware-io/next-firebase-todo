3198722e891ed2f36c4b983145ab0d5e264ef263
Reece Carolan <reececarolan@gmail.com>
2021-10-04 03:48:41 -0700

    Update nextjs and add nextjs lint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -59,6 +59,7 @@ MyDocument.getInitialProps = async (ctx) => {
 
   ctx.renderPage = () =>
     originalRenderPage({
+      // eslint-disable-next-line react/display-name
       enhanceApp: (App: any) => (props) =>
         <App emotionCache={cache} {...props} />,
     });
